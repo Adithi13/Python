3bf29a40b9a8c79aa4cd47beff8f92525c3843a4
Adithi13 <113155833+Adithi13@users.noreply.github.com>
2023-08-21 21:39:52 +0530

    Create Day1.py

---
+++ Day1.py
@@ -0,0 +1,12 @@
+#Given 3 variables i,d and s declred and initialized already. The task here is to declare 3 more variables of type int, type double and type string. Then by using "+" operator, print the sum of i + int variable,print the sum of d + double variable,concatenate s with the string read as input and print the result.
+
+CODE:
+i = 2
+d = 10.0
+s = 'Hello '
+a = int(input()) #if user input then int(input("enter a number"))
+b = float(input())#if user input then int(input("enter a number"))
+c = input()#if user input then input("enter a character")
+print(i + a)
+print(d + b)
+print(s + c)
